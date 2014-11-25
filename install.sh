d1b15058362a0909943f333f4bdd1fd7f571de62
Vishnu Atrai <me@vishnuatrai.com>
2014-11-25 14:31:41 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+# packages
+sudo apt-get install skype
+sudo apt-get install git
+sudo apt-get install zsh
+sudo apt-get install chromium-browser
+sudo apt-get install mysql-server-5.6 mysql-client-5.6
+sudo apt-get install postgresql-9.3 pgadmin3
+sudo apt-get install redis-server
+
+# mongodb latest stable install
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# change shell to zsh
+sudo chsh
+
+# configure dot files
+cd 
+mkdir src
+cd src
+git clone git@github.com:vatrai/dot_files.git
+
+ln -s ~/src/dot_files/zsh_profile.d ~/.zsh_profile.d
+ln -s ~/src/dot_files/zshrc ~/.zshrc
+ln -s ~/src/dot_files/irbrc ~/.irbrc
+ln -s ~/src/dot_files/gitconfig ~/.gitconfig
+ln -s ~/src/dot_files/gitignore ~/.gitignore
+ln -s ~/src/dot_files/gitk ~/.gitk
+mkdir ~/bin 
+ln -s ~/src/dot_files/sake ~/bin/sake
+ln -s ~/src/dot_files/vim ~/.vim
+ln -s ~/src/dot_files/vimrc ~/.vimrc
+ln -s ~/src/dot_files/aliasrc ~/.aliasrc
+ln -s ~/src/dot_files/gemrc ~/.gemrc
+ln -s ~/src/dot_files/bowerrc ~/.bowerrc
+ln -s ~/src/dot_files/grunt-init ~/.grunt-init
+
+
+echo "...completed..."
