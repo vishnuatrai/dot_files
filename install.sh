ea126d49f8e26925ea55b62c9874455bf7121b00
Vishnu Atrai <me@vishnuatrai.com>
2017-04-20 23:37:03 +0530

    docker install
    docker install

--- install.sh
+++ install.sh
@@ -72,3 +72,19 @@ nvm install 0.11
 nvm alias default 0.10.28
 
 echo "...completed..."
+
+#Docker Install
+sudo apt-get update
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+#Run docker without sudo
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
+sudo usermod -aG docker $USER
